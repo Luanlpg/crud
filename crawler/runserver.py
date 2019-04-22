09444eed3d3e2ac8fc87922ca40a6878a460ee56
luanlpg <luanlpg13@gmail.com>
2019-04-22 15:07:40 -0300

    5001

--- crawler/runserver.py
+++ crawler/runserver.py
@@ -15,4 +15,4 @@ def hello():
     return render_template("index.html")
 
 if __name__ == '__main__':
-            app.run(host='0.0.0.0', port=80, debug=True)
+            app.run(host='0.0.0.0', port=5001, debug=True)
